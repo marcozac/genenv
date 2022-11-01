7fb6079f17aca7c3a5a2a7e9bb4b45618b6e44be
marcozac <marco@marcozaccaro.com>
2022-11-01 19:15:08 +0100

    feat: add `ToPascal` method

---
+++ strconv.go
@@ -0,0 +1,56 @@
+package genenv
+
+import (
+	"strings"
+	"sync"
+	"unicode"
+)
+
+type StrConv struct {
+	mu sync.Mutex
+	B  *strings.Builder
+	l  int
+}
+
+func NewStrConv() *StrConv {
+	return &StrConv{
+		B: new(strings.Builder),
+	}
+}
+
+// ToPascal returns a pascal-cased copy of str.
+// Any space, hyphen, underscore, comma or dot is removed and the next
+// character is capitalized. Any other character is lowercase.
+//
+//	sc := NewStrConv()
+//	s := sc.ToPascal("FOO_-._BAR")
+//	// FooBar
+func (s *StrConv) ToPascal(str string) string {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.B.Reset()
+	if l := len(str) - s.l; l > 0 {
+		s.B.Grow(l)
+		s.l += l
+	}
+
+	u := true
+	for _, r := range str {
+		switch r {
+		case ' ', '-', '_', ',', '.':
+			u = true
+			continue
+		default:
+			if !u {
+				r = unicode.ToLower(r)
+				break // switch
+			}
+			r = unicode.ToUpper(r)
+			u = false
+		}
+		s.B.WriteRune(r)
+	}
+
+	return s.B.String()
+}
